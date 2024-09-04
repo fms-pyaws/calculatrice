401b084985043a5c6dabec616c6213a757223353
Theo Labadie <theo.labadie@fms-ea.com>
2024-09-04 10:57:30 +0200

    add multiply

--- calculator.py
+++ calculator.py
@@ -13,8 +13,11 @@ def add(op1, op2):
 def substract(op1, op2):
     return op1 - op2
 
+def multiply(op1, op2):
+    return op1 * op2
 
 if __name__ == '__main__':
     print(add(2, 3))
     print(substract(5, 2))
     print(substract(2, 5))
+    print(multiply(2, 5))
